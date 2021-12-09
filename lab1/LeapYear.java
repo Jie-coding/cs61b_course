19f5a479dc37e89deac3b0cbda2b4616cf3abb2a
Jie <949408742@qq.com>
2021-12-09 11:47:56 +0100

    Changed a bit

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -16,14 +16,17 @@ public class LeapYear {
 
     public static boolean isLeapYear(int year){
         /** Method that determines whether a year is a LeapYear or not. */
-        if (((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)){
+        if ((year%4 == 0) && (year%100 != 0)){
+            return true;
+        }
+        else if (year%400 == 0){
             return true;
         }
         else{
             return false;
         }
     }
-    
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
