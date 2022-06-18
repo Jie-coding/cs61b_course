86ba654b4b646658be88d42179e211967355b36f
Roger <rogerinchina23@gmail.com>
2022-06-18 18:45:00 +0800

    finish proj2b

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,141 @@
+package bearmaps;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.NoSuchElementException;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+
+    ArrayList<PriorityNode> heap;
+    TreeMap<T, Integer> itemMap;
+
+    private class PriorityNode {
+        private T item;
+        private double priority;
+
+        private PriorityNode(T item, double priority) {
+            this.item = item;
+            this.priority = priority;
+        }
+
+        private double getPriority() {
+            return this.priority;
+        }
+
+        private T getItem() {
+            return this.item;
+        }
+
+        private void setPriority(double priority) {
+            this.priority = priority;
+        }
+    }
+
+    public ArrayHeapMinPQ () {
+        heap = new ArrayList<>();
+        heap.add(null);  // We put the items starting from index 1.
+        itemMap = new TreeMap<>();
+    }
+
+    private int parent(int k) {
+        return k / 2;
+    }
+
+    private int leftChild(int k) {
+        return 2 * k;
+    }
+
+    private int rightChild(int k) {
+        return 2 * k + 1;
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if(contains(item)) {
+            throw new IllegalArgumentException(String.format("Item %s already exists in the heap!", item));
+        }
+        PriorityNode PNode = new PriorityNode(item, priority);
+        heap.add(PNode);
+        int index = heap.indexOf(PNode);
+        itemMap.put(PNode.getItem(), index);
+        swim(index);
+    }
+
+    private void swim(int k) {
+        while(parent(k) >= 1 && heap.get(parent(k)).getPriority() > heap.get(k).getPriority()) {
+            swap(parent(k), k);
+            k = parent(k);
+        }
+    }
+
+    private void swap(int a, int b) {
+        PriorityNode aNode = heap.get(a);
+        PriorityNode bNode = heap.get(b);
+        heap.set(a, bNode);
+        heap.set(b, aNode);
+        itemMap.put(aNode.getItem(), b);
+        itemMap.put(bNode.getItem(), a);
+    }
+    @Override
+    public boolean contains(T item) {
+        return itemMap.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        return heap.get(1).getItem();
+    }
+
+    @Override
+    public T removeSmallest() {
+        T smallest = getSmallest();
+        heap.set(1, heap.get(heap.size() - 1));
+        itemMap.remove(smallest);
+        sink(1);
+        heap.remove(heap.size() - 1);
+        return smallest;
+    }
+
+    private void sink(int k ) {
+        int smaller = smallerChild(k);
+        if (heap.get(smaller).getPriority() < heap.get(k).getPriority()) {
+            swap(smaller, k);
+            sink(smaller);
+        }
+    }
+
+    private int smallerChild(int k) {
+        int left = leftChild(k) < heap.size()? leftChild(k) : k;
+        int right = rightChild(k) < heap.size()? rightChild(k) : left;
+        return heap.get(right).getPriority() < heap.get(left).getPriority()? right: left;
+    }
+
+    @Override
+    public int size() {
+        return heap.size() - 1;
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException(String.format("Item %s does not exist in the heap!", item));
+        }
+        int idx = itemMap.get(item);
+        heap.get(idx).setPriority(priority);
+        if (parent(idx) >= 1 && heap.get(parent(idx)).getPriority() > heap.get(idx).getPriority()) {
+            swim(idx);
+        }
+        if (smallerChild(idx) < heap.size() && heap.get(smallerChild(idx)).getPriority() < heap.get(idx).getPriority()) {
+            sink(idx);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ArrayHeapMinPQ<String> heapMinPQ = new ArrayHeapMinPQ<>();
+
+        heapMinPQ.removeSmallest();
+        heapMinPQ.changePriority("d", 101);
+
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,126 @@
 package bearmaps;
 
+import edu.princeton.cs.introcs.StdRandom;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import edu.princeton.cs.algs4.Stopwatch;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+import static org.junit.Assert.*;
+
 
 public class ArrayHeapMinPQTest {
+
+    ArrayHeapMinPQ<String> PQ;
+    @Before
+    public void setup() {
+        PQ = new ArrayHeapMinPQ<>();
+        PQ.add("a", 3);
+        PQ.add("b", 4);
+        PQ.add("c", 2);
+        PQ.add("d", 4);
+        PQ.add("e", 5);
+        PQ.add("f", 100);
+        PQ.add("g", -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addExceptionTest() {
+        PQ.add("g", 1);
+    }
+
+    @Test
+    public void addSizeTest() {
+        assertEquals(PQ.size(), 7);
+        PQ.add("m", 100);
+        PQ.add("n", 1000);
+        PQ.contains("m");
+        assertEquals(PQ.size(), 9);
+    }
+
+    @Test
+    public void changePriorityTest() {
+        assertEquals(PQ.getSmallest(), "g");
+        PQ.changePriority("e", -1000);
+        assertEquals(PQ.removeSmallest(), "e");
+        assertEquals(PQ.removeSmallest(), "g");
+        assertEquals(PQ.removeSmallest(), "c");
+        assertEquals(PQ.removeSmallest(), "a");
+        assertTrue(PQ.contains("d"));
+        assertFalse(PQ.contains("e"));
+        assertEquals(PQ.removeSmallest(), "d");
+        assertEquals(PQ.removeSmallest(), "b");
+        assertEquals(PQ.removeSmallest(), "f");
+    }
+
+    @Test
+    public void getSmallestRandomTest() {
+        int num = 1000;
+        ArrayHeapMinPQ<Integer> PQ = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Integer> NPQ = new NaiveMinPQ<>();
+        Random random = new Random(100);
+        for (int i = 1; i < num; i += 1) {
+            int item = random.nextInt();
+            double randomP = random.nextDouble();
+            PQ.add(item,  randomP);
+            NPQ.add(item, randomP);
+            assertEquals(PQ.getSmallest(), NPQ.getSmallest());
+        }
+    }
+
+    @Test
+    public void changePriorityRandomTest() {
+        int num = 1000;
+        ArrayHeapMinPQ<Integer> PQ = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Integer> NPQ = new NaiveMinPQ<>();
+        Random random = new Random(100);
+        int[] items = new int[num];
+
+        for (int i = 0; i < num; i += 1) {
+            int item = random.nextInt();
+            double randomP = random.nextDouble();
+            items[i] = item;
+            PQ.add(item,  randomP);
+            NPQ.add(item, randomP);
+            assertEquals(PQ.getSmallest(), NPQ.getSmallest());
+        }
+        for (int i : items) {
+            double randomNumber = StdRandom.uniform(-1000,1000);
+            PQ.changePriority(i, randomNumber);
+            NPQ.changePriority(i, randomNumber);
+            assertEquals(PQ.getSmallest(), NPQ.getSmallest());
+        }
+    }
+
+    @Test
+    public void runningTimeTest() {
+        int exp = 6;
+        int base = 10;
+        int num = (int) Math.pow(base, exp);
+        ArrayHeapMinPQ<Integer> PQ = new ArrayHeapMinPQ<>();
+        NaiveMinPQ<Integer> NPQ = new NaiveMinPQ<>();
+        Random random = new Random(num);
+        for (int i = 0; i < num; i += 1) {
+            int item = i;
+            double randomP = random.nextDouble();
+            PQ.add(item,  randomP);
+            NPQ.add(item, randomP);
+        }
+
+        Stopwatch sw = new Stopwatch();
+        for (int i = 0; i < num/10; i += 1){
+            PQ.changePriority(i, i);
+        }
+        double timePQ = sw.elapsedTime();
+
+        Stopwatch sw2 = new Stopwatch();
+        for (int i = 0; i < num/10; i += 1) {
+            NPQ.changePriority(i, i);
+        }
+        double timeNPQ = sw2.elapsedTime();
+        System.out.printf("Time for HeapMinPQ is %.2f\n", timePQ);
+        System.out.printf("Time for NaiveMinPQ is %.2f\n", timeNPQ);
+    }
+
 }
